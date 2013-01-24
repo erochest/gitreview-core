4315f5ef74e8dcc3bf8437aa31639897a2eeebaf
Eric Rochester <erochest@virginia.edu>
2013-01-23 21:01:30 -0500

    Added specs.

---
+++ specs/GitReviewCoreSpecs.hs
@@ -0,0 +1,12 @@
+module Main where
+
+import Test.Hspec
+-- import Test.QuickCheck
+-- import Control.Exception (evaluate)
+
+main :: IO ()
+main = hspec $
+    describe "GitReviewCore" $ do
+        it "should have specs." $
+            pending "Add this."
+
