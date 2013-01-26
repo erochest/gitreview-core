a3253065f38a1a902756186d041b4872ca3b6d60
Eric Rochester <erochest@virginia.edu>
2013-01-25 23:05:37 -0500

    Filled in stubs for access functions.

--- Github/Review/Access.hs
+++ Github/Review/Access.hs
@@ -1,20 +1,37 @@
+{-# LANGUAGE RecordWildCards   #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module Github.Review.Access 
     ( getAccountRepos
     , getRepoCommits
-    , pickRandomN
+    , pickRandom
     ) where
 
-import Control.Error
-import Github.Data
-import Github.Review.Types
+import           Data.Monoid hiding (All)
+import qualified Data.Text as T
+import           Control.Error
+import           Github.Data
+import           Github.Repos
+import           Github.Repos.Commits
+import           Github.Review.Types
+import           System.Random.MWC
 
 getAccountRepos :: GithubAccount -> GithubInteraction [Repo]
-getAccountRepos = undefined
+getAccountRepos (GithubUserName name) = hoistGH $ userRepos name All
+getAccountRepos (GithubOrgName name)  = hoistGH $ organizationRepos name
 
 getRepoCommits :: Repo -> GithubInteraction [Commit]
-getRepoCommits = undefined
+getRepoCommits (Repo{..}) =
+        hoistGH $ commitsFor (githubOwnerLogin repoOwner) repoName
 
-pickRandomN :: Int -> [a] -> EitherT String IO [a]
-pickRandomN = undefined
+pickRandom :: [a] -> EitherT T.Text IO a
+pickRandom [] = left "No items to pick a random element from."
+pickRandom xs = do
+        n <- fmapLT textShow
+           . tryIO . withSystemRandom . asGenST
+           $ uniformR (0, length xs - 1)
+        hoistEither $ atErr ("Invalid random: " <> textShow n) xs n
+
+textShow :: Show a => a -> T.Text
+textShow = T.pack . show
 
