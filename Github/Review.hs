697a0d7765b7cf0c13f6925fac2a494677b23fde
Eric Rochester <erochest@virginia.edu>
2013-03-19 11:12:47 -0400

    Exported the version of the library.

--- Github/Review.hs
+++ Github/Review.hs
@@ -1,5 +1,6 @@
 module Github.Review
     ( module X
+    , version
     )
     where
 
@@ -7,4 +8,5 @@ import           Github.Review.Access  as X
 import           Github.Review.Filters as X
 import           Github.Review.Types   as X
 import           Github.Review.URI     as X
+import           Paths_gitreview_core  (version)
 
