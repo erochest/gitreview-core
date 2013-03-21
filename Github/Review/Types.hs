61b45fe29efd2ea9a73971157422605e59567756
Eric Rochester <erochest@virginia.edu>
2013-03-21 10:28:05 -0400

    Created a RetryT monad to use with GithubInteraction.

--- Github/Review/Types.hs
+++ Github/Review/Types.hs
@@ -1,10 +1,13 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 
 -- | This contains types for pulling a random commit out of Github, based on
 -- some simple chronological properties. That's a complicated way of saying
 -- that it picks a random, recent commit.
 
 module Github.Review.Types
-    ( GithubAccount(..)
+    ( RetryT
+    , runRetryT
+    , GithubAccount(..)
     , TaskName
     , TaskList
     , GithubInteraction
@@ -20,10 +23,13 @@ module Github.Review.Types
     )
     where
 
+import           Control.Applicative
 import           Control.Error
-import           Control.Monad.Trans (liftIO)
+import           Control.Monad.Trans
 import           Control.Monad.Writer
+import           Control.Retry
 import           Data.DList
+import           Data.Functor
 import qualified Data.Text as T
 import           Github.Data (Error(..), Repo, Commit)
 
@@ -33,16 +39,59 @@ data GithubAccount = GithubUserName String
 
 type TaskName = T.Text
 type TaskList = DList TaskName
-type GithubInteraction = EitherT Error (WriterT TaskList IO)
+
+-- | This runs the enclosed EitherT and retries according to RetrySettings
+-- whenever it returns a Left value.
+newtype RetryT e m a = Retrier
+                     { runRetryT :: m (Either e a)
+                     }
+
+-- TODO: Add ReaderT RetrySettings to the stack.
+
+bindRetry :: (Monad m, MonadIO m)
+          => RetryT e m a
+          -> (a -> RetryT e m b)
+          -> RetryT e m b
+x `bindRetry` f =
+        Retrier $ do
+            y <- retrying undefined isLeft $ runRetryT x
+            case y of
+                Right r  -> runRetryT $ f r
+                (Left l) -> return $ Left l
+
+insertRetry :: Monad m => a -> RetryT e m a
+insertRetry = Retrier . return . Right
+
+instance (Monad m, MonadIO m) => Monad (RetryT e m) where
+        x >>= f = x `bindRetry` f
+        return  = insertRetry
+
+instance (Monad m, MonadIO m) => MonadIO (RetryT e m) where
+        liftIO = lift . liftIO
+
+instance MonadTrans (RetryT e) where
+        lift = Retrier . (return . Right =<<)
+
+instance Functor m => Functor (RetryT e m) where
+        fmap f = Retrier . fmap (fmap f) . runRetryT
+
+instance (Monad m, Functor m) => Applicative (RetryT e m) where
+        pure    = Retrier . return . Right
+        f <*> x = Retrier $ do
+            f' <- runRetryT f
+            x' <- runRetryT x
+            return $ f' <*> x'
+
+type GithubInteraction = RetryT Error (WriterT TaskList IO)
 
 runGithubInteraction :: GithubInteraction a
                      -> IO (Either Error a, TaskList)
-runGithubInteraction = runWriterT . runEitherT
+runGithubInteraction = runWriterT . runRetryT
 
 type RepoCommit = (Repo, Commit)
 
 hoistGH :: IO (Either Error a) -> GithubInteraction a
-hoistGH = (hoistEither =<<) . lift . liftIO
+hoistGH = Retrier . lift . liftIO
 
 hoistEitherT :: EitherT Error IO a -> GithubInteraction a
 hoistEitherT = hoistGH . runEitherT
